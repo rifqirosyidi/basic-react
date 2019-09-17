2fca8ba5b61dfc0ac54fbc7327f28828d7e64ad5
Rifqi Rosyidi <rief.rosyidi@gmail.com>
2019-09-17 23:18:12 +0700

    components and props

--- src/App.js
+++ src/App.js
@@ -2,22 +2,26 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+function DisplayAge(props) {
+  return <p>{props.age} years old</p>
+}
+
+
+function Greeting(props) {
+  return <p>Hello {props.name} From Greeting, You Are
+    <DisplayAge age="25"/>
+  </p>;
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <Greeting name="Test User"/>
+
       </header>
     </div>
   );
