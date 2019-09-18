8425ab7daba3810e7c17ee0a8238bac443d2ae3c
Rifqi Rosyidi <rief.rosyidi@gmail.com>
2019-09-18 10:57:30 +0700

    basic state

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -14,13 +14,47 @@ function Greeting(props) {
   </p>;
 }
 
+class Timer extends Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+      time: props.start
+    }
+
+  }
+
+  componentDidMount() {
+    this.addInterval = setInterval( () => this.increase(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.addInterval);
+  }
+
+  increase() {
+    this.setState((state, props) => ({
+      time : parseInt(this.state.time) + 1
+    }));
+  }
+
+  render() {
+    return (
+      <div>
+        { this.state.time } Detik
+      </div>
+    )
+  }
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
 
-        <Greeting name="Test User"/>
+        <Greeting name="Test User" />
+        <Timer start="0" />
 
       </header>
     </div>
